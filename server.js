55b2645719799b7ad68c6997d70ae51578400cf6
aktivgo <AktivGo01062001@gmail.com>
2021-11-01 14:29:25 +0300

    Рефакторинг кода

--- server.js
+++ server.js
@@ -14,126 +14,6 @@ let onlineId = [];
 let activeSessions = new Map();
 let forbiddenWords = ['блять', 'сука', 'ебать', 'ебля', 'ебаный', 'ёбаный', 'пизда', 'долбоёб', 'хуй', 'хуёвый', 'хуевый'];
 
-server.on('connection', ws => {
-    ws.on('message', m => dispatchEvent(m, ws));
-    ws.on('error', e => ws.send(JSON.stringify({event: 'error', payload: e})));
-});
-
-const dispatchEvent = (message, ws) => {
-
-    const json = JSON.parse(message);
-    if (!json || !json.event || !json.payload) return;
-
-    switch (json.event) {
-
-        case 'connection': {
-            const token = json.payload.token;
-            if (!token) return;
-
-            let decoded;
-            try {
-                // Проверям корректность токена
-                decoded = jwt.verify(token, process.env.PRIVATE_KEY);
-            } catch (e) {
-                console.log('Error: ' + e);
-                ws.close();
-            }
-
-            const userId = decoded.userId;
-            const userName = decoded.userName;
-            if (!userId || !userName) return;
-
-            console.log('--------------------------------------------');
-            if (onlineId.indexOf(userId) === -1) {
-                // Добавляем пользователя в массив онлайн пользователей
-                addUserToOnlineList(userId, userName);
-                sendInfoMessage(userName, 'подключился к чату');
-                activeSessions.set(userId, 1);
-                console.log(userName + ' connected');
-            } else {
-                activeSessions.set(userId, activeSessions.get(userId) + 1)
-                console.log(userName + ' connected ' + activeSessions.get(userId) + ' time');
-            }
-
-            console.log('Online users: ' + onlineNames);
-            console.log('Active sessions: ' + activeSessions.get(userId));
-            console.log('--------------------------------------------');
-
-            // Подгружаем первую страницу сообщений из БД
-            sendMessagesFromDb(1, ws);
-            // Отсылаем клиентам список онлайн пользователей
-            sendOnlineList();
-        }
-            break;
-
-        case 'sendMessage': {
-            const username = json.payload.userName;
-            const message = json.payload.message;
-            const time = json.payload.time;
-            if (!username || !message || message === '' || !time) return;
-
-            saveMessage([username, message, time]);
-            sendMessage(username, censor(message), time);
-        }
-            break;
-
-        case 'getPageMessagesFromDb': {
-            const page = json.payload.page;
-            if (!page) return;
-
-            sendMessagesFromDb(page, ws);
-        }
-            break;
-
-        case 'disconnection': {
-            const userId = json.payload.userId;
-            const userName = json.payload.userName;
-            if (!userId || !userName) return;
-
-            console.log('--------------------------------------------');
-            if (isSingleActiveSession(userId)) {
-                deleteUserFromOnline(userId, userName);
-                sendOnlineList();
-                sendInfoMessage(userName, 'отключился от чата');
-                activeSessions.delete(userId)
-                ws.close();
-                console.log(userName + ' disconnected');
-            } else {
-                activeSessions.set(userId, activeSessions.get(userId) - 1)
-                console.log(userName + ' connected ' + activeSessions.get(userId) + ' time');
-            }
-
-            console.log('Online users: ' + onlineNames);
-            console.log('Active sessions: ' + (typeof activeSessions.get(userId) === 'undefined' ? 0 : activeSessions.get(userId)));
-            console.log('--------------------------------------------');
-
-        }
-            break;
-
-        case 'exit': {
-            const userId = json.payload.userId;
-            const userName = json.payload.userName;
-            if (!userId || !userName) return;
-
-            server.clients.forEach(client => {
-                client.send(JSON.stringify({event: 'checkToken', payload: {userName}}))
-            });
-
-            deleteUserFromOnline(userId, userName);
-            sendOnlineList();
-            sendInfoMessage(userName, 'отключился от чата');
-            activeSessions.delete(userId)
-
-            console.log('--------------------------------------------');
-            console.log(userName + ' disconnected');
-            console.log('Online users: ' + onlineNames);
-            console.log('Active sessions: ' + (typeof activeSessions.get(userId) === 'undefined' ? 0 : activeSessions.get(userId)));
-            console.log('--------------------------------------------');
-        }
-            break;
-    }
-}
-
 function connectToDb() {
     db = mysql.createConnection({
         host: process.env.MYSQL_HOST,
@@ -150,6 +30,108 @@ function connectToDb() {
     });
 }
 
+server.on('connection', ws => {
+    ws.on('message', m => dispatchEvent(m, ws));
+    ws.on('error', e => ws.send(JSON.stringify({event: 'error', payload: e})));
+});
+
+const dispatchEvent = (message, ws) => {
+
+    const json = JSON.parse(message);
+    if (!json || !json.event || !json.payload) return;
+
+    switch (json.event) {
+
+        case 'connection': {
+            connectionEvent(json.payload.token, ws);
+        }
+            break;
+
+        case 'sendMessage': {
+            sendMessageEvent(json.payload.userName, json.payload.message, json.payload.time);
+        }
+            break;
+
+        case 'getPageMessagesFromDb': {
+            getPageMessagesFromDbEvent(json.payload.page, ws);
+        }
+            break;
+
+        case 'disconnection': {
+            disconnectionEvent(json.payload.userId, json.payload.userName, ws);
+        }
+            break;
+
+        case 'exit': {
+            exitEvent(json.payload.userId, json.payload.userName);
+        }
+            break;
+    }
+}
+
+/*
+ Событие подключения
+ */
+function connectionEvent(token, ws) {
+    if (!token) return;
+
+    let decoded;
+    try {
+        // Проверям корректность токена
+        decoded = jwt.verify(token, process.env.PRIVATE_KEY);
+    } catch (e) {
+        console.log('Error: ' + e);
+        ws.close();
+    }
+
+    const userId = decoded.userId;
+    const userName = decoded.userName;
+    if (!userId || !userName) return;
+
+    console.log('--------------------------------------------');
+    if (onlineId.indexOf(userId) === -1) {
+        // Добавляем пользователя в массив онлайн пользователей
+        addUserToOnlineList(userId, userName);
+        sendInfoMessage(userName, 'подключился к чату');
+        activeSessions.set(userId, 1);
+        console.log(userName + ' connected');
+    } else {
+        activeSessions.set(userId, activeSessions.get(userId) + 1)
+        console.log(userName + ' connected ' + activeSessions.get(userId) + ' time');
+    }
+
+    console.log('Online users: ' + onlineNames);
+    console.log('Active sessions: ' + activeSessions.get(userId));
+    console.log('--------------------------------------------');
+
+    // Подгружаем первую страницу сообщений из БД
+    sendMessagesFromDb(1, ws);
+    // Отсылаем клиентам список онлайн пользователей
+    sendOnlineList();
+}
+
+function addUserToOnlineList(id, name) {
+    if (!id || !name) return;
+
+    onlineNames.push(name);
+    onlineId.push(id);
+}
+
+/*
+ Событие отправки сообщения
+ */
+function sendMessageEvent(userName, message, time) {
+    if (!userName || !message || message === '' || !time) return;
+
+    saveMessage([userName, message, time]);
+    sendMessage(userName, censor(message), time);
+}
+
+function saveMessage(data) {
+    db.query("insert into messages  (id, userName, message, time) values (null, ?, ?, ?)", data, function (err, results) {
+    });
+}
+
 function sendMessage(userName, message, time) {
     if (!userName || !message || !time) return;
 
@@ -157,46 +139,29 @@ function sendMessage(userName, message, time) {
         event: 'sendMessage',
         payload: {userName, message, time}
     })));
-    console.log('Message sand to clients');
 }
 
-function censor(userMessage) {
-    if (!userMessage) return;
+function censor(message) {
+    if (!message) return;
 
-    const words = userMessage.split(' ');
+    const words = message.split(' ');
     for (let i in words) {
         let word = words[i].toLowerCase();
         if (forbiddenWords.indexOf(word) !== -1) {
-            userMessage = userMessage.replace(words[i], '*'.repeat(word.length))
+            message = message.replace(words[i], '*'.repeat(word.length))
         }
     }
-    return userMessage;
+    return message;
 }
 
-function saveMessage(data) {
-    db.query("insert into messages  (id, userName, message, time) values (null, ?, ?, ?)", data, function (err, results) {
-    });
-    console.log('Message saved to Db');
-}
 
-function isSingleActiveSession(userId) {
-    if (!userId) return false;
+/*
+ Событие получения страницы сообщений из БД
+ */
+function getPageMessagesFromDbEvent(page, ws) {
+    if (!page) return;
 
-    return activeSessions.get(userId) === 1;
-}
-
-function addUserToOnlineList(userId, userName) {
-    if (!userId || !userName) return;
-
-    onlineNames.push(userName);
-    onlineId.push(userId);
-}
-
-function deleteUserFromOnline(userId, userName) {
-    if (!userId || !userName) return;
-
-    onlineNames.splice(onlineId.indexOf(userId), 1);
-    onlineId.splice(onlineId.indexOf(userId), 1);
+    sendMessagesFromDb(page, ws);
 }
 
 function sendMessagesFromDb(page, ws) {
@@ -209,6 +174,65 @@ function sendMessagesFromDb(page, ws) {
     db.query("select * from messages ORDER BY id desc limit ?, ?", [offset, limit], function (err, results) {
         ws.send(JSON.stringify({event: event, payload: {results}}));
     });
+}
+
+
+/*
+ Событие отключения
+ */
+function disconnectionEvent(userId, userName, ws) {
+    if (!userId || !userName) return;
+
+    console.log('--------------------------------------------');
+    if (isSingleActiveSession(userId)) {
+        deleteUserFromOnline(userId, userName);
+        sendOnlineList();
+        sendInfoMessage(userName, 'отключился от чата');
+        activeSessions.delete(userId)
+        ws.close();
+        console.log(userName + ' disconnected');
+    } else {
+        activeSessions.set(userId, activeSessions.get(userId) - 1)
+        console.log(userName + ' connected ' + activeSessions.get(userId) + ' time');
+    }
+
+    console.log('Online users: ' + onlineNames);
+    console.log('Active sessions: ' + (typeof activeSessions.get(userId) === 'undefined' ? 0 : activeSessions.get(userId)));
+    console.log('--------------------------------------------');
+}
+
+function isSingleActiveSession(userId) {
+    return activeSessions.get(userId) === 1;
+}
+
+
+/*
+ Событие выхода
+ */
+function exitEvent(userId, userName) {
+    if (!userId || !userName) return;
+
+    server.clients.forEach(client => {
+        client.send(JSON.stringify({event: 'checkToken', payload: {userName}}))
+    });
+
+    deleteUserFromOnline(userId, userName);
+    sendOnlineList();
+    sendInfoMessage(userName, 'отключился от чата');
+    activeSessions.delete(userId)
+
+    console.log('--------------------------------------------');
+    console.log(userName + ' disconnected');
+    console.log('Online users: ' + onlineNames);
+    console.log('Active sessions: ' + (typeof activeSessions.get(userId) === 'undefined' ? 0 : activeSessions.get(userId)));
+    console.log('--------------------------------------------');
+}
+
+function deleteUserFromOnline(userId, userName) {
+    if (!userId || !userName) return;
+
+    onlineNames.splice(onlineId.indexOf(userId), 1);
+    onlineId.splice(onlineId.indexOf(userId), 1);
 }
 
 function sendOnlineList() {
